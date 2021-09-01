4ea34f47cd011e1c95e17659d3f80d7d42010230
ostylk <ostylk@googlemail.com>
2021-09-01 13:45:55 +0200

    pkgs: retrieve pinned nixpkgs from flake

--- pkgs.nix
+++ pkgs.nix
@@ -1,9 +1,3 @@
-let
-  sha256 = "sha256:1wg55jlxyvbjvm8x2rcirmvqws4y8xq504dn3yjp05m1bajhpj5r";
-  rev = "6daa4a5c045d40e6eae60a3b6e427e8700f1c07f";
-in
-builtins.trace "(Using pinned Nixpkgs at ${rev})"
-import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-  inherit sha256;
-})
+import (import (fetchTarball https://github.com/edolstra/flake-compat/archive/master.tar.gz) {
+  src = builtins.fetchGit ./.;
+}).defaultNix.inputs.nixpkgs
