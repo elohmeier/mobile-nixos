88996e3ccdce534ea9f13d4eaa75649ae84b1b1e
Enno Richter <enno@nerdworks.de>
2022-01-17 16:28:43 +0100

    pine64-pinephone: update kernel 5.13.7 -> 5.15.13

--- devices/pine64-pinephone/kernel/default.nix
+++ devices/pine64-pinephone/kernel/default.nix
@@ -6,15 +6,15 @@
 }:
 
 mobile-nixos.kernel-builder {
-  version = "5.13.7";
+  version = "5.15.13";
   configfile = ./config.aarch64;
   src = fetchFromGitHub {
     # https://github.com/megous/linux
     owner = "megous";
     repo = "linux";
-    # orange-pi-5.13
-    rev = "b1f383fd3c70e21a8062fdc3f59403d2a26edc9f";
-    sha256 = "0m1jhkq6lxp12a302r80qbz8l1sdmqnwfymg1lx68c91dm7j3wkf";
+    # orange-pi-5.15-20220105-2352
+    "rev"= "59ae79cd1b201529b77336f3badeace5258426e1";
+    "sha256"= "vueckfhiekLyRETgPsItUjYq8tIWKDCe8feQd4qUN5I=";
   };
   patches = [
     ./0001-dts-pinephone-Setup-default-on-and-panic-LEDs.patch
